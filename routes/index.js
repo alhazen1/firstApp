2568f0458fe48ebc42e89b79e36b3fe13134988e
d wilder <thereal.david.wilder@gmail.com>
2018-09-27 19:13:51 -0500

    Played with handlebars

--- routes/index.js
+++ routes/index.js
@@ -3,7 +3,7 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Cool!!!' });
+  res.render('index', { title: 'Cool!!!' , condition:false, anyArray:[1,2,3]});
 });
 
 module.exports = router;
